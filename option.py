be88140b11264b330916adaa80850f835077ed6b
HuQian <86952915+HuQ1an@users.noreply.github.com>
2024-03-01 19:36:08 +0800

    Add files via upload

--- option.py
+++ option.py
@@ -6,10 +6,10 @@ parser.add_argument('--template', default='mst',
                     help='You can set various templates in option.py')
 
 # Hardware specifications
-parser.add_argument("--gpu_id", type=str, default='0')
+parser.add_argument("--gpu_id", type=str, default='0,1,2,3')
 
 # Data specifications
-parser.add_argument('--data_root', type=str, default='./dataset/', help='dataset directory')
+parser.add_argument('--data_root', type=str, default='../../datasets/', help='dataset directory')
 
 # Saving specifications
 parser.add_argument('--outf', type=str, default='./exp/mst_s/', help='saving_path')
@@ -20,14 +20,24 @@ parser.add_argument('--pretrained_model_path', type=str, default=None, help='pre
 parser.add_argument("--input_setting", type=str, default='H',
                     help='the input measurement of the network: H, HM or Y')
 parser.add_argument("--input_mask", type=str, default='Phi',
-                    help='the input mask of the network: Phi, Phi_PhiPhiT or None')
+                    help='the input mask of the network: Phi, Phi_PhiPhiT, Mask or None')  # Phi: shift_mask   Mask: mask
 
+# Training specifications
+parser.add_argument('--batch_size', type=int, default=20, help='the number of HSIs per batch')
+parser.add_argument("--max_epoch", type=int, default=300, help='total epoch')
+parser.add_argument("--scheduler", type=str, default='MultiStepLR', help='MultiStepLR or CosineAnnealingLR')
+parser.add_argument("--milestones", type=int, default=[50,100,150,200,250], help='milestones for MultiStepLR')
+parser.add_argument("--gamma", type=float, default=0.5, help='learning rate decay for MultiStepLR')
+parser.add_argument("--epoch_sam_num", type=int, default=5000, help='the number of samples per epoch') # 5000
+parser.add_argument("--learning_rate", type=float, default=0.001)
 opt = parser.parse_args()
 template.set_template(opt)
 
-opt.mask_path = f"{opt.data_root}/Mask/"
-opt.test_path = f"{opt.data_root}/Truth/"
+# dataset
 
+opt.data_path = f"{opt.data_root}/cave_1024_28/"
+opt.mask_path = f"{opt.data_root}/TSA_simu_data/"
+opt.test_path = f"{opt.data_root}/TSA_simu_data/Truth/"
 for arg in vars(opt):
     if vars(opt)[arg] == 'True':
         vars(opt)[arg] = True
